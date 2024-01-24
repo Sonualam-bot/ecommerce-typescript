f19dbd62705a67ef5c5de38c4e34389230957232
Sonualam <sonualam1599@gmail.com>
2024-01-24 23:46:18 +0530

    added Product card

---
+++ src/components/Product-cart.tsx
@@ -0,0 +1,37 @@
+import { FaPlus } from "react-icons/fa";
+
+type ProudctProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+
+const server = "jsjasdksjlasl";
+
+const ProductCard = ({
+  productId,
+  photo,
+  name,
+  price,
+  stock,
+  handler,
+}: ProudctProps) => {
+  return (
+    <div className="product-card">
+      <img src={photo} alt="name" />
+      <p>{name}</p>
+      <span>₹{price}</span>
+      <div>
+        <button onClick={() => handler()}>
+          {" "}
+          <FaPlus />{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import ProductCard from "../components/Product-cart";
 
 function Home() {
+  const addToCarthandler = () => {};
   return (
     <div className="home">
       <section></section>
@@ -10,7 +12,16 @@ function Home() {
           More
         </Link>
       </h1>
-      <main></main>
+      <main>
+        <ProductCard
+          productId="adslkasd"
+          name="Mackbook"
+          price={4545}
+          stock={435}
+          handler={addToCarthandler}
+          photo="https://m.media-amazon.com/images/I/71S4sIPFvBL._SX679_.jpg"
+        />
+      </main>
     </div>
   );
 }
