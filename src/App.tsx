690ffdbea0e526dca6cb2fa425e4eb380e49226d
Sonualam <sonualam1599@gmail.com>
2024-01-24 22:44:02 +0530

    added hero image section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/loader";
+import Header from "./components/header";
 
 const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
@@ -29,6 +30,7 @@ function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,16 @@
-import Header from "../components/header";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <Header />
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+      <main></main>
     </div>
   );
 }
