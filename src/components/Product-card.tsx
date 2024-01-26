3ffa32f7eca1f0e24c2a603711ccc344f863b3a0
Sonualam <sonualam1599@gmail.com>
2024-01-26 23:31:38 +0530

    style: added animation to product card

--- src/components/Product-card.tsx
+++ src/components/Product-card.tsx
@@ -20,17 +20,24 @@ const ProductCard = ({
   handler,
 }: ProudctProps) => {
   return (
-    <div className="product-card">
-      <img src={photo} alt="name" />
-      <p>{name}</p>
-      <span>₹{price}</span>
-      <div>
-        <button onClick={() => handler()}>
-          {" "}
-          <FaPlus />{" "}
-        </button>
-      </div>
-    </div>
+    <figcaption className="product-card">
+      <img
+        src="https://pixabay.com/get/g7a1ab4e1da0dd6375251480a7a496d0574f92d926a287009b1206a18c6e342391db7ea1379343ab37ffed80b0850b0391d019e3f6b66c9c17c13339dfb91f14f_640.jpg"
+        alt="/proudct-image"
+      />
+      <section>
+        <main>
+          <h2>{name}</h2>
+          <p>₹{price}</p>
+        </main>
+        <footer>
+          <button onClick={() => handler()}>
+            {" "}
+            <FaPlus />{" "}
+          </button>
+        </footer>
+      </section>
+    </figcaption>
   );
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import ProductCard from "../components/Product-card";
-// import mackbook from "../assets/images/mackbook.jpg";
 
 function Home() {
   const addToCarthandler = () => {};
@@ -14,6 +13,22 @@ function Home() {
         </Link>
       </h1>
       <main>
+        <ProductCard
+          productId="adslkasd"
+          name="Mackbook"
+          price={4545}
+          stock={435}
+          handler={addToCarthandler}
+          photo="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHx8MA%3D%3D"
+        />
+        <ProductCard
+          productId="adslkasd"
+          name="Mackbook"
+          price={4545}
+          stock={435}
+          handler={addToCarthandler}
+          photo="https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHx8MA%3D%3D"
+        />
         <ProductCard
           productId="adslkasd"
           name="Mackbook"
